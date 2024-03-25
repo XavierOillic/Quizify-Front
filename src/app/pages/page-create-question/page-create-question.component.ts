39839fea833475d5e893bc4233b19b870bd7a51e
EloElodie18 <elodierichard@yahoo.fr>
2024-03-25 21:23:45 +0100

    init page question/reponse

---
+++ src/app/pages/page-create-question/page-create-question.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-create-question',
+  templateUrl: './page-create-question.component.html',
+  styleUrls: ['./page-create-question.component.css']
+})
+export class PageCreateQuestionComponent {
+
+}
