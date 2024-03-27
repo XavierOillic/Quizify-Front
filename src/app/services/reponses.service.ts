3f9af6be13adeae481859a3f1cd5cff9c14de34c
Xavier Oillic <xavieroillic@gmail.com>
2024-03-27 10:51:41 +0100

    services reponse OK

--- src/app/services/reponses.service.ts
+++ src/app/services/reponses.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Reponse } from '../models/reponse';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReponsesService {
+  constructor(private Http: HttpClient) {}
 
-  constructor() { }
+  urlApi = 'http://localhost:8080/reponse';
+
+  getAllCat(): Observable<Reponse[]> {
+    return this.Http.get<Reponse[]>(this.urlApi);
+  }
+
+  getOne(reponseId: number): Observable<Reponse> {
+    return this.Http.get<Reponse>(`${this.urlApi}/${reponseId}`);
+  }
+
+  createNewCategorie(reponseToCreate: Reponse): Observable<Reponse> {
+    return this.Http.post<Reponse>(this.urlApi, reponseToCreate);
+  }
+
+  updateCat(reponseUpdated: Reponse, reponseId: number): Observable<Reponse> {
+    return this.Http.put<Reponse>(
+      `${this.urlApi}/${reponseId}`,
+      reponseUpdated
+    );
+  }
+
+  delete(reponseId: number) {
+    return this.Http.delete<Reponse>(`${this.urlApi}/${reponseId}`);
+  }
 }
